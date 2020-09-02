a5271790b867da4b3b6c08d73e860386920e0bea
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-09-02 19:54:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ client.on('message', msg => {
             let msgArgs = args.slice(2).join(" ");
             
             msg.channel.send("📋 "+"**"+`New Suggestion from ${user.tag}:`+msgArgs+"**").then(messagereaction => {
-                messagereaction.react("👍");
-                messagereaction.react("👎");
+                messagereaction.react("🥵");
+                messagereaction.react("🥶");
                 msg.delete();
             })
 
