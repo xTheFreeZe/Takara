c8324c3a11e85aeff32deb95543c407a345a13ec
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-09-02 13:11:07 +0200

    consol log fix

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ client.on('message', msg => {
             
             msg.channel.send(msgArgs).then(messagereaction => {
                 msg.delete();
-                console.log(`In deleted the message of ${member} `)
+                console.log(`In deleted the message of ${user.tag} `)
             })
 
         break;
