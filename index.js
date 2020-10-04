c09ddd67a48224034da964b4540b32e1bb7e22d7
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-10-04 11:18:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -181,8 +181,8 @@ client.on('message', msg => {
             let msgArgs = args.slice(1).join(" ");
 
             msg.channel.send("**" + msgArgs + "**").then(messagereaction => {
-                messagereaction.react("↑");
-                messagereaction.react("↑");
+                messagereaction.react("🔼");
+                messagereaction.react("🔼");
                 msg.delete();
             })
 
