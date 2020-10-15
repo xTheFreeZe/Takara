8e14bf6b6aaa1c4a16f3e7c27756fa0ce9057ab2
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-10-15 14:33:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -788,7 +788,7 @@ client.on('message', msg => {
             .setTitle('Our twitter Team')
             .setColor('#E42162')
             .setThumbnail('https://cdn.discordapp.com/attachments/685794100112392212/750020815034122350/STT_BOT_PREMIUM_2.png')
-            .setDescription('Sign up here if you want to be part of our Overwatch Tournaments! Click here (https://www.youtube.com/watch?v=fHI8X4OXluQ&list=RD9wMirJXQEAY&index=3&ab_channel=TheWeekndVEVO)')
+            .setDescription('Sign up here if you want to be part of our Overwatch Tournaments! [Click here] (https://www.youtube.com/watch?v=fHI8X4OXluQ&list=RD9wMirJXQEAY&index=3&ab_channel=TheWeekndVEVO)')
         msg.channel.send(embed);
 
     }
