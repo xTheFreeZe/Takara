3c075c75f56b21eb1f3b200b0b94fc5d6be6b100
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-10-22 22:59:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -514,14 +514,15 @@ client.on('message', msg => {
     if (msg.content === "^ping") {
         var ping = client.ws.ping;
         const embed = new MessageEmbed()
-        .setColor('RANDOM')
-        .setDescription(":ping_pong: Ping of STT Premium is `" + `${ping}` + " ms`")
+            .setColor('RANDOM')
+            .setDescription(":ping_pong: Ping of STT Premium is `" + `${ping}` + " ms`")
         msg.channel.send('Checking for ping... Hold on')
             .then((msg) => {
                 setTimeout(function () {
                     msg.edit(embed);
                 }, 4000)
             });
+        msg.delete();
     }
 })
 
