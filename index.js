4aeb74a138632a2ab6296cc6b25a47150d12a3a7
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-11-17 17:44:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -897,13 +897,13 @@ client.on('message', message => {
 client.on('message', message => {
     if (message.channel instanceof Discord.DMChannel) return;
     if (message.author.bot) return;
-    if (!message.member.roles.cache.has('714096868178788414')) return message.reply("You can't use that");
     let args = message.content.substring(PREFIX.length).split(" ");
     let msgArgs = args.slice(2).join(" ");
     let author = message.author
 
     switch (args[0]) {
         case 'ban':
+            if (!message.member.roles.cache.has('735498148629446657')) return message.reply("You can't use that");
 
 
 
