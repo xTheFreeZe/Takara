8a5dd0c4b8af6d0a7d5a52b15add39f01951f6de
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-11-18 08:54:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -215,7 +215,7 @@ client.on("message", msg => {
         const embed = new MessageEmbed()
             .setImage("https://cdn.discordapp.com/attachments/681060754564448257/778525893857378314/cdf09b00aea778cb509aafc4cccc4e77.png")
             .setTitle("Welcome to the Server!")
-            .setDescription ^ ("Welcome to `Ghol's Couch`!")
+            .setDescription("Welcome to `Ghol's Couch`!")
             .addField('Owner', '<@!409080789435809802>', true)
             .addField('Admin', '<@!420277395036176405>', true)
         msg.channel.send(embed);
