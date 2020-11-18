49cabbd092fc9bb0ff7fe9cfd6d2471f3c6ed292
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-11-18 08:53:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -211,14 +211,14 @@ client.on('message', msg => {
 client.on("message", msg => {
     let args = msg.content.substring(PREFIX.length).split(" ");
     let msgArgs = args.slice(1).join(" ");
-    if(msg.content === "^welcome new") {
+    if (msg.content === "^welcome new") {
         const embed = new MessageEmbed()
-        .setImage("https://cdn.discordapp.com/attachments/681060754564448257/778525893857378314/cdf09b00aea778cb509aafc4cccc4e77.png")
-        .setTitle("Welcome to the Server!")
-        .setDescription^("Welcome to `Ghol's Couch`!")
-        .addField('Owner', '<@!409080789435809802>', true)
-        .addField('Admin', '<@!420277395036176405>')
-    msg.channel.send(embed);
+            .setImage("https://cdn.discordapp.com/attachments/681060754564448257/778525893857378314/cdf09b00aea778cb509aafc4cccc4e77.png")
+            .setTitle("Welcome to the Server!")
+            .setDescription ^ ("Welcome to `Ghol's Couch`!")
+            .addField('Owner', '<@!409080789435809802>', true)
+            .addField('Admin', '<@!420277395036176405>', true)
+        msg.channel.send(embed);
     }
 })
 
