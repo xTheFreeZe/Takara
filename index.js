2cec2ff4ca6c345704fc7e24cf4181dfb1b21e9d
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-11-21 13:08:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -285,7 +285,7 @@ client.on('message', msg => {
 
     switch (args[0]) {
         case "hug":
-            if(!args[1]) return ("Use more Args");
+            if(!args[1]) return msg.reply("Use more Args");
             const user = msg.mentions.users.first();
             const member = msg.guild.member(user);
 
