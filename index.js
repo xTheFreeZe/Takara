e7c258bef4720360e2095afb15fc8b77a09575fb
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-11-24 20:25:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -864,6 +864,14 @@ client.on('message', message => {
                             .setDescription(`<:STT_yes:778545433810173952> STT Premium kicked ${member} Reason:` + " " + "**" + msgArgs + "**")
                             .setTimestamp()
                         message.reply(embed);
+                        let logembed = new MessageEmbed()
+                        .setColor("RANDOM")
+                        .setDescription(`${member} got warned`)
+                        .addField(`Moderator:`, `${author}`)
+                        .addField(`Channel: `, `${channel}`)
+                        .addField(`Reason:`, msgArgs)
+                        .setTimestamp()
+                        log_channel.send(logembed);
                         log_channel.send("I kicked someone");
                         console.log(`I kicked ${user.tag}. Provided Reason:` + msgArgs);
                         message.delete();
