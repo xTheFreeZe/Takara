a28e8e6cc22c3e134904799eb304d417397b8106
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-12-05 10:33:11 +0100

    Update index.js

--- index.js
+++ index.js
@@ -133,6 +133,32 @@ client.on('message', msg => {
     }
 })
 
+client.on("message", msg => {
+    if (msg.content === "^commands") {
+        const embed = new MessageEmbed()
+            .setTitle("STT Premium")
+            .setColor("#e2b007")
+            .setThumbnail("https://cdn.discordapp.com/attachments/681060754564448257/784713060552146995/STT_BOT_PREMIUM_2.png")
+            .setDescription("All Commands :")
+            .addField("Genearl commands ", "`^help` , `^help fun` , `^help mod` , `^help dev`")
+            .addField("Test commands", "`^delay 10` , `^delay 20`")
+            .addField("Info commands", "`^info help` , `^info ping` , `^help join` ,`^info kick` , `^info suggest` , `^info warn`")
+            .addField("Chat commands", "`^suggest [suggestion]` , `^pred [team vs team ] ` , `^hug [person you want to hug`")
+            .addField("Chat command", "`^speak [your message ] ` ")
+            .addField("Staff commands", "`^warn [@member] [reason] ` , `^topic`")
+            .addField("Staff commands", "`^kick [@member] [reason] `  `^ban [@member] [reason] `")
+            .addField("Bug command", "`^report [your report ] `")
+            .addField("Fun commands", "`^ping` , `^online` , `^avatar`")
+            .addField("Help staff", "`^help suggest` , `^help ban`")
+            .addField("Voice Channel commands", "`^join` , `^leave` , `^play`")
+            .addField("Socials", "`^twitter` , `^twitter2` ,  `^website`")
+            .addField("ID Command", "`^ID [@member]`")
+            .addField("Thank you for using my Bot!!", "Marwin")
+            .setFooter("Last edit : 05.12.2020")
+        msg.channel.send(embed);
+    }
+})
+
 client.on('message', msg => {
     if (msg.content === "^delay 10") {
         const embed = new MessageEmbed()
