755ebdaf6fa54aff527d81079cb1a1c5f2b72f66
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-12-16 15:58:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1315,16 +1315,6 @@ client.on('message', msg => {
 })
 
 
-client.on("message", msg => {
-    if (msg.content === "owner") {
-        let owner = msg.author.id == '420277395036176405';
-        if (!owner) return msg.channel.send(`Only the owner can use this command`);
-        msg.reply("heyy");
-
-
-    }
-})
-
 client.on('message', msg => {
     if (msg.content.includes("^talk")) {
         let user = msg.mentions.users.first();
