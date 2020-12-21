ec75d079a652033d889ea91e19262dc668f8fed2
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-12-21 11:25:46 +0100

    test command

--- commands/test/test.js
+++ commands/test/test.js
@@ -3,11 +3,21 @@ module.exports = {
     category: "test",
     description: "description",
     run: async (client, message, args) => {
-
-        if (message.content === "test") {
-            message.reply("Hello!");
-        }
-
+        if (!args[1]) return message.channel.send(`:STT_no: ${author} You need to use a second argument. Example **^report [bug]**`);
+        const user = msg.mentions.users.first();
+        const member = msg.guild.member(user);
+        let msgArgs = args.slice(1).join(" ");
+        const embed = new MessageEmbed()
+            .setDescription(`:STT_yes: Your Report has been sent to <@!420277395036176405> :` + " " + "**" + msgArgs + "**")
+            .setColor('RANDOM')
+        message.channel.send(embed);
+        message.channel.send("Connection didn't time out and everything worked! Message got sent to `420277395036176405` !");
+        let report_embed = new MessageEmbed()
+            .setDescription(`New Report from ${author} in ${channel} from ${msg.guild}:` + " " + msgArgs)
+            .setColor("RANDOM")
+        client.users.cache.get('420277395036176405').send(report_embed);
+        console.log(`New Report from ${author}:` + msgArgs);
+        message.delete();
 
     }
 }
