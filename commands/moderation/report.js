bd2e48ee9997de7f753f2d17e31a4e6f23569127
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-12-21 11:51:41 +0100

    Update report.js

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -8,13 +8,14 @@ module.exports = {
     run: async (client, message, args) => {
         let author = message.author
         let channel = message.channel
+        let args = msg.content.substring(PREFIX.length).split(" ");
 
         if (!args[0]) return message.channel.send(`:STT_no: ${author} You need to use a second argument. Example **^report [bug]**`);
         const user = message.mentions.users.first();
         const member = message.guild.member(user);
         let msgArgs = args.slice(1).join(" ");
         const embed = new MessageEmbed()
-            .setDescription(`:STT_yes: Your Report has been sent to <@!420277395036176405> :` + " " + "**" + msgArgs + "**")
+            .setDescription(`<:STT_no:778545452218974209> Your Report has been sent to <@!420277395036176405> :` + " " + "**" + msgArgs + "**")
             .setColor('RANDOM')
         message.channel.send(embed);
         message.channel.send("Connection didn't time out and everything worked! Message got sent to `420277395036176405` !");
