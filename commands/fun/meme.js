91b3f92f9d377f81779b1c1b5f5839f11478c84a
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-12-22 11:24:50 +0100

    new meme command

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -1,6 +1,7 @@
 const {
     MessageEmbed
 } = require('discord.js');
+const Discord = require('discord.js');
 module.exports = {
     name: "meme",
     category: "fun",
--- index.js
+++ index.js
@@ -642,29 +642,6 @@ client.on("message", msg => {
 })
 
 
-client.on("message", message => {
-    if (message.content === "^meme") {
-        const embed = new Discord.MessageEmbed()
-        got('https://www.reddit.com/r/memes/random/.json').then(response => {
-            let content = JSON.parse(response.body);
-            let permalink = content[0].data.children[0].data.permalink;
-            let memeUrl = `https://reddit.com${permalink}`;
-            let memeImage = content[0].data.children[0].data.url;
-            let memeTitle = content[0].data.children[0].data.title;
-            let memeUpvotes = content[0].data.children[0].data.ups;
-            let memeDownvotes = content[0].data.children[0].data.downs;
-            let memeNumComments = content[0].data.children[0].data.num_comments;
-            embed.setTitle(`${memeTitle}`)
-            embed.setURL(`${memeUrl}`)
-            embed.setImage(memeImage)
-            embed.setColor('RANDOM')
-            embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
-            message.channel.send(embed);
-        })
-    }
-
-})
-
 client.on("message", msg => {
     if (msg.content === "^online") {
         var ping = client.ws.ping;
