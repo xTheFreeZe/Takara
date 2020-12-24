c48d107460de62a265d3780060a271d43590412b
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2020-12-24 11:27:26 +0100

    Update index.js

--- index.js
+++ index.js
@@ -528,30 +528,6 @@ client.on('message', msg => {
     }
 })
 
-//warn command: ^warn (@member) {reason}
-
-
-client.on("message", msg => {
-    if (msg.content === "^uptime") {
-        if (msg.channel instanceof Discord.DMChannel) return;
-        if (msg.author.bot) return;
-        let totalSeconds = (client.uptime / 1000);
-        let days = Math.floor(totalSeconds / 86400);
-        totalSeconds %= 86400;
-        let hours = Math.floor(totalSeconds / 3600);
-        totalSeconds %= 3600;
-        let minutes = Math.floor(totalSeconds / 60);
-        let seconds = Math.floor(totalSeconds % 60);
-        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
-        const embed = new MessageEmbed()
-            .setDescription(`The bot has been online for ${uptime}`)
-            .setColor('RANDOM')
-            .setFooter('Powered by STT Productions')
-            .setTimestamp()
-        msg.channel.send(embed);
-
-    }
-})
 
 client.on("message", msg => {
     if (msg.content === "^system") {
