4114743d1ebf4f8315a3a7c15da21c2d3aa626d2
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-01-03 22:10:30 +0100

    Create kill.js

---
+++ commands/fun/kill.js
@@ -0,0 +1,27 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+module.exports = {
+    name: "kill",
+    category: "fun",
+    description: "Sends a funny way of killing someone...",
+
+    run: async (client, message, PREFIX) => {
+        const user = message.mentions.users.first() || msg.author;
+
+        const messages = [
+            `${message.author.username} kills ${user.username} with a knif 🔪 🩸 `,
+            `${user.username} dies because of a car accident 🚗`,
+            `During a party ${user.username} had a heart attack and died because if it... sagde ☹`,
+            `Bruh...${user.username} just died...☠`,
+            `${user.username} fell from a high place 🏙`,
+            `Looks like ${user.username} played too much fortnite and just died because of it 💻`,
+            `Someone did drink too much juice and ${user.username} passed out and died 🥤`
+            `${message.author.username} kills ${user.username} during an intense gun battle 🧨 🔫`
+        ]
+
+        const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+
+        message.channel.send(randomMessage)
+    }
+}
