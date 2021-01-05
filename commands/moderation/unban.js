bdd59fe57c148e3248bd9c2c0deae487ee3bab03
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-01-05 10:28:59 +0100

    Create unban.js

---
+++ commands/moderation/unban.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: "unban",
+    category: "moderation",
+    description: "unbans a member from a guild",
+
+    run: async (client, message, PREFIX, args) => {
+        const member = args[0]
+
+
+        if (!member) {
+            return message.channel.send("Please enter a valid user ID!");
+
+        }
+
+        try {
+            message.guild.fetchBans().then(bans => {
+                message.guild.members.unban(member)
+            })
+            await message.channel.send(`${member} has been unbanned`);
+
+        } catch (e) {
+            return message.channel.send("Something has happened and I could not unban this Member");
+        }
+
+    }
+}
