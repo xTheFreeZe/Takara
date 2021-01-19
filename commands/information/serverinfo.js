2a8514e3d7a4eeac6d2fb9f4acb06169bd249f52
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-01-19 09:11:03 +0100

    New Command : ^serverinfo

---
+++ commands/information/serverinfo.js
@@ -0,0 +1,22 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+    name: "serverinfo",
+    category: "information",
+    description: "Shows stats for the current server",
+    run: async (client, message, PREFIX) => {
+        const embed = new MessageEmbed()
+            .setTitle('Server Info')
+            .setColor("RANDOM")
+            .addField('Server name:', `${message.guild.name}`)
+            .addField('Total members:', `${message.guild.memberCount}`)
+            .addField('Owner:', `${message.guild.owner}`)
+            .addField("Roles Count", `This server has ${message.guild.roles.cache.size} roles`)
+            .addField("Emoji Count", `This server has ${message.guild.emojis.cache.size} emojis`)
+            .setThumbnail(`${message.guild.iconURL()}`)
+            .setFooter('STT Premium | Information')
+        message.channel.send(embed);
+    }
+}
