69a634ba38f5bff17a3cec839893570ea343b34e
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-01-23 10:05:43 +0100

    Update restart.js

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "restart",
     category: "owner",
     run: async (client, message, args) => {
-        if (message.author.id !== '185957154606284800') {
+        if (message.author.id !== '420277395036176405') {
             return message.channel.send(`You cannot use this command!`)
         }
         await message.channel.send(`Restarting bot...`)
