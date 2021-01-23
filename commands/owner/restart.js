5564989b127c7e36816acf7a2900502a972f0741
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-01-23 10:12:14 +0100

    Update restart.js

--- commands/owner/restart.js
+++ commands/owner/restart.js
@@ -1,11 +1,21 @@
+const {
+    MessageEmbed
+} = require('discord.js');
 module.exports = {
     name: "restart",
     category: "owner",
     run: async (client, message, args) => {
         if (message.author.id !== '420277395036176405') {
-            return message.channel.send(`You cannot use this command!`)
+            const permsembed = new MessageEmbed()
+                .setDescription('<:STT_no:778545452218974209> You can not use this command!')
+                .setColor("RANDOM")
+
+            return message.channel.send(permsembed)
         }
-        await message.channel.send(`Restarting bot...`)
+        const reembed = new MessageEmbed()
+            .setDescription('<:STT_yes:778545433810173952> Restarting bot... Please wait!')
+            .setColor('#229954')
+        await message.channel.send(reembed)
         process.exit();
     }
 }
