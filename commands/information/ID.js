68cf72aea0bfe7320dc8d2b969fb8286e97110e5
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-02-01 13:12:05 +0100

    Update ID.js

--- commands/information/ID.js
+++ commands/information/ID.js
@@ -8,9 +8,9 @@ module.exports = {
     description: "shows Discord ID of someone",
     run: async (client, message, PREFIX) => {
         let args = message.content.substring(PREFIX.length).split(" ");
-        let user = message.mentions.users.first();
+        let user = message.mentions.users.first() || message.author;
         let author = message.author
-        let member = message.mentions.members.first() || message.author
+        let member = message.mentions.members.first() || message.author;
         const embed = new MessageEmbed()
             .setDescription(`This is the Discord ID from ${member}: ` + " " + "**" + user + "**")
             .setFooter("STT Premium | Information")
