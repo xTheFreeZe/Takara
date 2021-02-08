3d6b0ede090217248ab1ef49b618495547ee8d11
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-02-08 13:40:21 +0100

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -28,14 +28,6 @@ module.exports = {
             .setDescription("<:STT_no:778545452218974209> You can't ban yourself!")
             .setColor("RANDOM")
 
-        const DMembed = new MessageEmbed()
-            .setDescription("<:STT_yes:778545433810173952> You have been banned!")
-            .addField('Server :', `${message.guild}`)
-            .addField('Moderator :', `${message.author.tag}`)
-            .addField('Reason :', msgArgs)
-            .setThumbnail(`${message.guild.iconURL()}`)
-            .setTimestamp()
-
         let log_channel = message.guild.channels.cache.get('780815502997454848');
         if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply(permsembed);
         if (!args[1]) return message.channel.send(argsembed);
@@ -56,6 +48,15 @@ module.exports = {
                         .setTimestamp()
                         .setFooter('STT Premium | Moderation')
                     message.channel.send(embed);
+
+                    const DMembed = new MessageEmbed()
+                        .setDescription("<:STT_yes:778545433810173952> You have been banned!")
+                        .addField('Server :', `${message.guild}`)
+                        .addField('Moderator :', `${message.author.tag}`)
+                        .addField('Reason :', msgArgs)
+                        .setThumbnail(`${message.guild.iconURL()}`)
+                        .setTimestamp()
+
                     member.send(DMembed);
                     console.log(`I banned ${user.tag} on ${message.guild}. Provided Reason:` + msgArgs);
                     message.delete();
