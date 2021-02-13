098dd7431f41c7acfa889ba42757598ec798180f
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-02-13 16:21:21 +0100

    Update topic.js

--- commands/moderation/topic.js
+++ commands/moderation/topic.js
@@ -10,27 +10,36 @@ module.exports = {
     run: async (client, message) => {
         let author = message.author
         let channel = message.channel
+        const log_channel = message.guild.channels.cache.find(r => r.name === 'logs');
+
         let permsembed = new MessageEmbed()
             .setDescription(`<:STT_no:778545452218974209> ${message.author.username} you can't use that!`)
             .addField("Error", 'Missing `KICK_MEMBERS`')
             .setColor("RANDOM")
+
+        const nologembed = new MessageEmbed()
+            .setDescription("<:STT_no:778545452218974209> Please create a channel called `logs` before using this command!")
+            .setColor("RANDOM")
+
         if (!message.member.hasPermission('KICK_MEMBERS')) return message.delete(), message.reply(permsembed);
+        if (!log_channel) return message.channel.send(nologembed);
         if (message.channel instanceof Discord.DMChannel) return;
         if (message.author.bot) return;
-        //let log_channel = message.guild.channels.cache.get('780815502997454848');
-        //let logembed = new MessageEmbed()
-        //  .setDescription("**TOPIC**")
-        // .addField('Moderator', `${author}`)
-        //.addField('Channel', `${channel}`)
-        //.setThumbnail(msg.author.displayAvatarURL())
-        //.setColor("RANDOM")
-        //.setTimestamp()
-        //log_channel.send(logembed);   
         const embed = new MessageEmbed()
             .setTitle('Watch out!')
             .setColor('#EC0808')
             .setDescription('Please make sure your conversation is relevant to the current channel!')
         message.channel.send(embed);
+
+        const logembed = new MessageEmbed()
+            .setColor("RANDOM")
+            .setTitle(`TOPIC`)
+            .addField('Moderator', `${message.author.tag}`)
+            .addField('Channel', `${message.channel}`)
+            .setTimestamp()
+
+        log_channel.send(logembed);
+
         message.delete();
     }
 }
