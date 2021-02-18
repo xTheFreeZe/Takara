8fe1e653b6cf25a52ce64a68179ff2fe2d9e4d74
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-02-18 15:14:03 +0100

    Update ping.js

--- commands/information/ping.js
+++ commands/information/ping.js
@@ -1,5 +1,6 @@
 const {
-    MessageEmbed, Message
+    MessageEmbed,
+    Message
 } = require('discord.js');
 
 module.exports = {
@@ -7,13 +8,17 @@ module.exports = {
     category: "information",
     description: "Ping of the Bot",
     run: async (client, message) => {
+
         var ping = client.ws.ping;
+
+        let highping = ping > 150;
+
         const embed = new MessageEmbed()
             .setColor('RANDOM')
             .setDescription(":ping_pong: Ping of STT Premium is `" + `${ping}` + " ms`")
             .addField("`My Status`", "[Click here](https://sttproductions.statuspage.io/)", true)
         message.channel.send(embed);
-        if (ping > 150) message.channel.send('High Ping detected!');
+        if (highping) message.channel.send('High Ping detected! This could lead to slow messages. If the ping isnt at 90 - 110 in an Hour, please check out the Status Page of the Bot too see if everything is normal!');
         message.delete();
     }
 }
