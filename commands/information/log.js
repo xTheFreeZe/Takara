8a6671814621601a2a5fea16f787f535cf382bf9
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-02-22 20:33:25 +0100

    new command : ^log
    This command can be used to check if there is a log channel! The Bot will log every mod action in  the log channel. Using any mod commands without a logchannel doesnt work!

---
+++ commands/information/log.js
@@ -0,0 +1,27 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+module.exports = {
+    name: "log",
+    category: "information",
+    description: "checks if a log channel is there",
+    run: async (client, message, args) => {
+        const log_channel = message.guild.channels.cache.find(r => r.name === 'log')
+
+        const logyes = new MessageEmbed()
+            .setColor('#00FF00')
+            .setDescription('<:STT_yes:778545433810173952> There is a log channel!')
+            .setFooter('All Mod commands can be used!')
+
+        const logno = new MessageEmbed()
+            .setColor("#FF0000")
+            .setDescription('<:STT_no:778545452218974209> There is **no** log channel!')
+            .setFooter('No Mod commands can be used!')
+
+        if (!log_channel) return message.channel.send(logno);
+
+        log_channel.send(`This is a test message, see ${message.channel}!`);
+        
+        message.channel.send(logyes);
+    }
+}
