167894cce43d9de1ef43ab292819f7a605fb12a9
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-02-23 19:27:14 +0100

    Update ban.js

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -69,8 +69,7 @@ module.exports = {
                     ression: msgArgs
                 }).then(() => {
                     const embed = new MessageEmbed()
-                        //.setDescription(`<:STT_yes:778545433810173952> STT Premium banned  ${member} Reason:**` + " " + msgArgs + "**")
-                        .setDescription(`<:STT_yes:778545433810173952> STT Premium banned  ${member}` + reason)
+                        .setDescription(`<:STT_yes:778545433810173952> STT Premium banned  ${member} Reason:**` + " " + msgArgs + "**")
                         .setColor('#229954')
                         .setTimestamp()
                         .setFooter('STT Premium | Moderation')
