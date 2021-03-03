0dc7b4c7d22b4f3c0156d325416b363700e95716
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-03-03 14:36:21 +0100

    Update hug.js

--- commands/fun/hug.js
+++ commands/fun/hug.js
@@ -10,17 +10,13 @@ module.exports = {
         let args = message.content.substring(PREFIX.length).split(" ");
         let author = message.author
         const argsembed = new MessageEmbed()
-            .setDescription(`<:STT_no:778545452218974209> ${message.author.username} please mention someone.`)
+            .setDescription(`<:STT_no:778545452218974209> ${message.author.username} please mention someone you want to hug.`)
             .setColor("RANDOM")
         if (!args[1]) return message.channel.send(argsembed);
         const user = message.mentions.users.first();
         const member = message.guild.member(user);
 
-
-        if (!args[1]) {
-            message.channel.send('Use ^hug {@person you want to hug} ');
-            message.delete();
-        }
+        if (!user) return message.channel.send(argsembed);
 
         let msgArgs = args.slice(1).join(" ");
         const embed = new MessageEmbed()
