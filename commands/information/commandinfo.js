2fd975f7766a77bdcd697068f44f648627d54a21
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-04-02 18:21:02 +0200

    Update commandinfo.js

--- commands/information/commandinfo.js
+++ commands/information/commandinfo.js
@@ -13,12 +13,26 @@ module.exports = {
             .setDescription('Choose a command you want to learn more about.')
             .addField('Available Commands', '`ban`, `kick`, `lock`, `purge`, `report`, `topic`, `unban`, `warn`')
             .setFooter('Type ^info [command] to see the information')
+            .setColor('GREEN')
+        
+        
+        const banembed = new MessageEmbed()
+            .setTitle('Information: Ban')
+            .addField('Name', '^ban')
+            .addField('Category', 'moderation')
+            .addField('Description', 'Bans a member from a guild')
+            .addField('Cooldown', 'No Cooldown')
 
 
 
         if (!args[0]) {
 
             return message.channel.send(infoembed);
+        
+        } else if (args[0] == 'ban') {
+
+            return message.channel.send(banembed);
+
         }
 
 
