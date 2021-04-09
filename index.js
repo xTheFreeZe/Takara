e28562748aae8421538ca9f89306feeca8b4be6b
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-04-09 12:04:55 +0200

    update : welcome message

--- index.js
+++ index.js
@@ -143,6 +143,76 @@ client.on('guildCreate', (guild) => {
 
 })
 
+const applyText = (canvas, text) => {
+    const ctx = canvas.getContext('2d');
+
+
+    let fontSize = 70;
+
+    do {
+
+        ctx.font = `${fontSize -= 10}px sans-serif`;
+
+    } while (ctx.measureText(text).width > canvas.width - 300);
+
+    return ctx.font;
+};
+
+
+
+client.on('guildMemberAdd', async member => {
+
+    const channel = member.guild.channels.cache.find(channel => channel.name === "welcome");
+
+    if (!channel) return;
+
+
+    const canvas = Canvas.createCanvas(700, 250);
+
+    const ctx = canvas.getContext('2d');
+
+    const background = await Canvas.loadImage('./STT_background.png');
+
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.strokeStyle = '#74037b';
+
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+
+    ctx.font = '28px sans-serif';
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+
+    ctx.font = applyText(canvas, `${member.displayName}!`);
+    ctx.fillStyle = '#ffffff';
+    ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
+
+    ctx.beginPath();
+
+    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
+
+    ctx.closePath();
+
+    ctx.clip();
+
+    const avatar = await Canvas.loadImage(member.user.displayAvatarURL({
+        format: 'jpg'
+    }));
+
+    ctx.drawImage(avatar, 25, 25, 200, 200);
+
+    const attachement = new Discord.MessageAttachment(canvas.toBuffer(), 'stt-logo.jpg');
+
+
+
+    channel.send(`Welcome ${member}`, attachement);
+})
+
+
+
 
 
 client.on("message", msg => {
