c2691ba197ae149f33155bbdbbaaf021981402bf
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-04-25 16:43:26 +0200

    Update exit.js

--- commands/owner/exit.js
+++ commands/owner/exit.js
@@ -18,17 +18,17 @@ module.exports = {
 
         var server = message.guild
 
-        message.channel.send('Leaving... Bye!').catch((e) => {
+        message.channel.send('Leaving... Bye!');
+        message.delete();
+
+        console.log(`Bot left ${message.guild.name} `)
+
+        server.leave().catch((e) => {
 
             //catches error when leaving a server
 
             console.log(e);
 
         })
-        message.delete();
-
-        console.log(`Bot left ${message.guild.name} `)
-
-        server.leave()
     }
 }
