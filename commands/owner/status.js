924a7df559c5b2038bc68e57bc240846642302fe
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-06-04 19:34:00 +0200

    :star: Cooldown :star:

--- commands/owner/status.js
+++ commands/owner/status.js
@@ -3,6 +3,8 @@ const {
     NewsChannel
 } = require('discord.js');
 
+const servercheck = new Set();
+
 module.exports = {
     name: "status",
     description: "change the status of the bot",
@@ -17,23 +19,38 @@ module.exports = {
             return message.channel.send(permsembed);
         }
 
-        let args = message.content.substring(PREFIX.length).split(" ");
-        let newStatus = args.slice(1).join(" ");
+        if (servercheck.has(message.guild.id)) {
+            const timemebed = new MessageEmbed()
+                .setDescription("<:STT_no:778545452218974209> To prevent issus, this command has a **5 Minute** Cooldown!")
+                .setColor("#FF0000")
+            message.channel.send(timemebed);
 
-        const newStatusembed = new MessageEmbed()
-            .setTitle('Success!')
-            .setDescription('<:STT_yes:778545433810173952>  Changed status to playing' + " " + "**" + newStatus+ "**")
-            .setColor('GREEN')
+        } else {
 
-        if (!newStatus) return message.channel.send('Please provide a status!');
+            let args = message.content.substring(PREFIX.length).split(" ");
+            let newStatus = args.slice(1).join(" ");
+
+            const newStatusembed = new MessageEmbed()
+                .setTitle('Success!')
+                .setDescription('<:STT_yes:778545433810173952>  Changed status to playing' + " " + "**" + newStatus + "**")
+                .setColor('GREEN')
+
+            if (!newStatus) return message.channel.send('Please provide a status!');
 
 
 
-        client.user.setActivity(newStatus).catch(() => {
-            return message.channel.send('A random error occured!');
-        })
+            client.user.setActivity(newStatus).catch(() => {
+                return message.channel.send('A random error occured!');
+            })
 
-        message.channel.send(newStatusembed);
+            message.channel.send(newStatusembed);
+
+            servercheck.add(message.guild.id);
+            setTimeout(() => {
+                talkedRecently.delete(message.guild.id);
+            }, 300000);
+
+        }
 
     }
 }
