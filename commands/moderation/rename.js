b0b41f5ed9ab611e9772ed631fd9da40d8b39447
xTheFreeZe <66825996+xTheFreeZe@users.noreply.github.com>
2021-06-07 17:06:33 +0200

    new command : ^rename :star:

---
+++ commands/moderation/rename.js
@@ -0,0 +1,54 @@
+const {
+    MessageEmbed
+} = require('discord.js');
+
+module.exports = {
+    name: "rename",
+    category: "moderation",
+    description: "rename a mentioned user!",
+
+    run: async (client, message, PREFIX) => {
+
+        const user = message.mentions.users.first();
+        let args = message.content.substring(PREFIX.length).split(" ");
+        let newName = args.slice(2).join(" ");
+
+        const noArgsone = new MessageEmbed()
+            .setTitle('Oops, you are missing something!')
+            .setColor('RED')
+            .setDescription('<:STT_no:778545452218974209> Please mention someone and provide a new Nickname for them!')
+
+        const noUser = new MessageEmbed()
+            .setTitle('Oops, you are missing something!')
+            .setColor('RED')
+            .setDescription('<:STT_no:778545452218974209> Please mention someone!')
+
+        const noNewName = new MessageEmbed()
+            .setTitle('Oops, you are missing something!')
+            .setColor('RED')
+            .setDescription('<:STT_no:778545452218974209> Please provide a new Nickname!')
+
+        if (!args[1]) {
+
+            return message.channel.send(noArgsone);
+
+        }
+
+        if (!user) {
+
+            return message.channel.send(noUser);
+
+        }
+
+        if (!newName) {
+
+            return message.channel.send(noNewName);
+
+        }
+
+        user.setNickname(newName, `${message.author.username} renamed ${user.username} with ^rename`).catch(() => {
+            message.channel.send('error');
+        })
+
+    }
+}
